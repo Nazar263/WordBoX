c19a19e980e51fd4b2495b7dcce190e51bae471f
Nazar263 <marcaknazar370@gmail.com>
2025-12-19 19:57:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function white1() {
+ let phone = document.getElementById("call");
+  phone.classList.remove("black");
+  phone.classList.add("white")
+}
+function black1(){
+let phone2 = document.getElementById("call");
+ phone2.classList.remove("white");
+ phone2.classList.add("black");
+}
+function flex() {
+ let flex1 = document.getElementById("left_dictionary");
+ flex1.classList.add("flex")
+ 
+}
